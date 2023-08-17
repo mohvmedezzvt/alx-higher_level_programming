1bcc9f1fbd2fd7c16f384be1c149a3d526be440b
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-08-17 22:24:45 +0300

    Added a function that prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: pointer to the head of the doubly linked list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
