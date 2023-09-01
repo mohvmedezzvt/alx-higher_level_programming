3cbde73a380969001191d4ffa2a7977d37b7c312
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-09-01 17:24:14 +0300

    Added a function that executes a function safely.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,9 +4,7 @@ from sys import stderr
 
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
+        return fct(*args)
     except Exception as e:
         print("Exception: {}".format(e), file=stderr)
         return None
-
-    return (result)
