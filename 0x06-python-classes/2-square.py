743c87f7bc8d89a37898009b8b694e00fb3609aa
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-09-02 16:05:08 +0300

    Added a class Square that defines a square by Private instance attribute: size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+2-square Module
+
+This module defines a basic square shape represented by the `Square` class.
+It currently serves as a template for creating square objects.
+"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
