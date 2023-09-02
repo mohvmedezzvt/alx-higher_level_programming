6b30344c655c0ffb821d8dfc893e3c4631fecc43
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-09-02 16:15:26 +0300

    Added a class Square that defines a square by Private instance attribute: size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 
 """
-1-square Module
+2-square Module
 
 This module defines a basic square shape represented by the `Square` class.
 It currently serves as a template for creating square objects.
-
-Example usage:
->>> Square = __import__('0-square').Square
->>> my_square = Square(3)
->>> type(square)
-<class '__main__.Square'>
 """
 
 
 class Square:
+    """
+    Square class with a private instance attribute __size.
+    """
+
     def __init__(self, size=0):
         """
         Initializes a new Square instance.
