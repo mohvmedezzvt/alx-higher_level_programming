eedb1fae4286638062d8752b4b3a77618ea4a35d
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-09-02 15:33:49 +0300

    Added an empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,9 +3,13 @@ class Square:
     """
     Square
 
-    An Empty Class representing a Square.
+    This class represents a basic square shape. It does not contain any
+    methods or attributes yet. It serves as a template for creating square
+    objects.
 
     Example usage:
-    >>> my_square = Square()
+    >>> square = Square()
+    >>> type(square)
+    <class '__main__.Square'>
     """
     pass
