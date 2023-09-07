5bc5e526352f416bc43df1efb41a05fd18172869
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-09-07 03:24:48 +0300

    Added a class Rectangle that defines a rectangle by Private instance attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
             width (int, optional): The width of the Rectangle. Defaults to 0.
             height (int, optional): The height of the Rectangle. Defaults to 0.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
