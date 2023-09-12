fa1f6f41e27cfcad26ec248e77ad8c63a7a6a3f0
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-09-12 05:34:13 +0300

    Added a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module that contains MyList Class.
+"""
+
+
+class MyList(list):
+    """a class MyList that inherits from list
+
+    Args:
+        list (list): list to print
+    """
+    def print_sorted(self):
+        """Print the list in ascending sorted order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
