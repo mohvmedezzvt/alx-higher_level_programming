42684b76cc7fc244d6a1ef14210fc4805d8417b4
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-09-12 07:26:29 +0300

    Added Public instance method: def area(self):

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a Module that contains BaseGeometry Class.
+"""
+
+
+class BaseGeometry():
+    """A Class BaseGeometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
