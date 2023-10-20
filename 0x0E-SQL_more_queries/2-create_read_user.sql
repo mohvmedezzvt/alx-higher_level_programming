817c7f3ff04dea37917f1c7be24e8815b7170765
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-10-20 15:19:59 +0300

    a script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,10 +2,11 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create the user user_0d_2.
-CREATE USER IF NOT EXISTS 'user_0d_2'@'loclahost'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant SELECT privilege on the hbtn_0d_2 database.
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
+-- flush privileges to apply the changes.
 FLUSH PRIVILEGES;
