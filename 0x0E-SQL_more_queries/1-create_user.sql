ca2c9dc08f4b8471a90943758ab4911439bbfd83
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-10-20 12:57:41 +0300

    a script that creates the MySQL server user user_0d_1.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,3 +5,6 @@ IDENTIFIED BY 'user_0d_1_pwd';
 -- grant all privileges to the user.
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+
+-- reload the privilege tables and apply the changes immediately.
+FLUSH PRIVILEGES;
