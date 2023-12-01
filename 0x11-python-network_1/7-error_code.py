f2d63a6270eb4f400e22791198c644df32589e21
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-12-02 01:17:31 +0200

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes in a URL,
+Sends a request to the URL and
+Displays the body of the response.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.text)
+
+    if r.status_code // 100 == 4:
+        print(f"Error code: {response.status_code}")
