abc5da615d3547fdb9b16478e8f399fcabf24b4c
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-12-02 17:23:37 +0200

    a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL
-
-url="$1"
-filename="$2"
-
-# checks if the file is not valid JSON
-if ! jq . "$filename" > /dev/null 2>&1; then
-    echo "Not a valid JSON"
-    exit 1
-fi
-
-curl -sX POST -H "Content-Type: application/json" --data @"$filename" "$url"
+curl -sX POST -H "Content-Type: application/json" --data @"$2" "$1"
